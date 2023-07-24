13990f51c65b89360ecaefd549146d0941a79635
Daud Abdul Rehman <daud.arehman@gmail.com>
2023-07-24 18:09:25 +0500

    weeek1 practice

---
+++ index.js
@@ -0,0 +1,58 @@
+// let user = {
+//     name : "daud",
+//     address : "LHR",
+
+//     speak: function speak(){
+//        console.log("speaking")
+//        return 2
+//     }
+// }
+
+// //console.log("=================user-------------",user.speak())
+// const speakmethod = user.speak
+// console.log("========speakmethod=======",speakmethod())
+
+//spread 
+//loadash library
+
+
+//day3
+
+// const numbers = [1,23,78,89,90]
+// const numbers2 = [...numbers]
+// console.log(numbers2)
+
+// const user = {
+//     name: "daud",
+//     address:{
+//         town:"judicial 3",
+//         street:"five"
+//     }
+// }
+
+// const user2 = {...user}
+// console.log(user2)
+
+// // object destructuring
+// let {name} = user
+// console.log("=============name===============",name)
+
+
+// const {address} = user
+// console.log("=============address===============",address)
+// address.town = "eme"
+// console.log("=============address===============",address)
+// name = "mansoor"
+// console.log("==============username=========",user.name)
+
+// //name is assigned by value and address is assigned by reference
+
+// //array destructuring(see at home)
+
+
+//=============================================================DAY 5==============================================================
+//day 5(friday)
+
+
+
+
